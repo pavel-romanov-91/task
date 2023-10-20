afc753ef7347bfce23684623b6c4c9cfe87762ef
telegramstore <pavel-romanov-91@mail.ru>
2023-10-20 19:12:06 +0300

    dsg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
