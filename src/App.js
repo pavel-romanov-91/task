d7aaf4e527c008b523732c058d06eac3dc895ba3
telegramstore <pavel-romanov-91@mail.ru>
2023-10-25 19:36:47 +0300

    q

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import Nav from './components/Nav/Nav';
 
 
 function App() {
+//   const our_data = ['Item1', 'Item2', 'Item3', 'Iten4']
+//  const [var1,var2,var3,var4] = our_data;
+//   const handleClick = ()=>console.log(var1,var2,var3,var4);
+// onClick={handleClick}
   return (
     <div>
-      <Header logo_src={'/img/content_thumb.png'}/>
+      <Header logo_src={'/img/content_thumb.png'} />
       <Nav menu_items={[{title:"Новости", link:"/news"},{title:"О нас", link:"/about"}]}/>
       <Footer contacts={'Наши Контакты'} copy={'Наши Коперайты'}/>
     </div>
